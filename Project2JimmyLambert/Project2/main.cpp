3ff24ffb1f41c3a69f2dc5cba90ff86a5d3d7b3c
james <mistcurve@outlook.com>
2019-10-14 13:00:17 -0400

    Provided framework for all the scripts in main.cpp

--- Project2JimmyLambert/Project2/main.cpp
+++ Project2JimmyLambert/Project2/main.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+void pushScript();
+void popScript();
+void topScript();
+void unionScript();
+void printScript();
+void saveScript();
+
 int main()
 {
 	int response = 0;
@@ -12,7 +19,65 @@ int main()
 		cout << PROMPT;
 		cin >> response;
 
+		switch (response)
+		{
+		case 1:
+			pushScript();
+			break;
+		case 2:
+			popScript();
+			break;
+		case 3:
+			topScript();
+			break;
+		case 4:
+			unionScript();
+			break;
+		case 5:
+			printScript();
+			break;
+		case 6:
+			saveScript();
+			break;
+		case 7:
+			response = -1;
+			break;
+		default:
+			response = 0;
+			break;
+		}
+
 	} while (response != -1);
 
 	return 0;
+}
+
+void pushScript()
+{
+	cout << "Executing pushScript...";
+}
+
+void popScript()
+{
+	cout << "Executing popScript...";
+}
+
+void topScript()
+{
+	cout << "Executing topScript...";
+}
+
+void unionScript()
+{
+	cout << "Executing unionScript";
+}
+
+void printScript()
+{
+	cout << "Executing printScript";
+}
+
+void saveScript()
+{
+	cout << "Executing saveScript";
 }
