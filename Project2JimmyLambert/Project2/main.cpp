e0ea583e56c36fb9dabca1ee9ad3ad7ed322daf7
Saber Davidson <mistcurve@outlook.com>
2019-10-15 23:56:12 -0400

    Added some stuffies

--- Project2JimmyLambert/Project2/main.cpp
+++ Project2JimmyLambert/Project2/main.cpp
@@ -94,20 +94,56 @@ void popScript(SStack* stack)
 
 void topScript(SStack* stack)
 {
-	cout << "Executing topScript...";
+	cout << "Executing topScript..." << endl;
+	cout << "Displaying the top item of the stack:" << endl;
+	Employee e = stack->pop();
+	cout << e;
 }
 
 void unionScript(SStack* stack)
 {
 	cout << "Executing unionScript";
+	Employee e = Employee(0101, "James", "Lambert");
+	Stack s0 = Stack(5);
+	s0.push(e);
+	e = Employee(1020, "Saber", "Davidson");
+	s0.push(e);
+	e = Employee(3829, "John", "Cena");
+	s0.push(e);
+	Stack s1 = Stack(5);
+	e = Employee(3304, "Big", "Mac");
+	s1.push(e);
+	e = Employee(8362, "David", "Tennant");
+	s1.push(e);
+	e = Employee(1123, "Micheal", "Sheen");
+	s1.push(e);
+	e = Employee(1173, "Neil", "Gaiman");
+	s1.push(e);
+	cout << "Combining the two following stacks into one stack..." << endl;
+	cout << "Stack1:" << endl << s0 << endl;
+	cout << "Stack2:" << endl << s1 << endl;
+	Stack s2 = s0 + s1;
+	cout << "Stack3, Union of Stack1 and Stack2:" << endl << s2;
 }
 
 void printScript(SStack* stack)
 {
 	cout << "Executing printScript";
+	cout << "Now printing the current stack:" << endl;
+	stack->print();
 }
 
 void saveScript(SStack* stack)
 {
 	cout << "Executing saveScript";
+	ofstream outfile;
+	outfile.open("out_jameslambert.txt", ios::out);
+	cout << "Outputting the contents of the current stack to out_jameslambert.txt" << endl;
+	while(stack->size > 0)
+	{
+		Employee e = stack->pop();
+		outfile << e;
+	}
+	outfile.close();
+
 }
